868279bfcbb09053bab748c3f04377bd30f9a1b4
Long Nguyen <long.nalou@gmail.com>
2017-02-26 10:37:08 +0700

    implement Business page

---
+++ Yelp/Yelp/Cells/BusinessCell.swift
@@ -0,0 +1,47 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Nguyen Nam Long on 2/25/17.
+//  Copyright © 2017 CoderSchool. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var restaurantNameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var reviewImageView: UIImageView!
+    @IBOutlet weak var reviewCountLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var categoriesLabel: UILabel!
+    
+    
+    var business: Business! {
+        didSet {
+            avatarImageView.setImageWith(business.imageURL!)
+            restaurantNameLabel.text = business.name
+            distanceLabel.text = business.distance
+            reviewImageView.setImageWith(business.ratingImageURL!)
+            reviewCountLabel.text = "\(business.reviewCount!) Reviews"
+            addressLabel.text = business.address
+            categoriesLabel.text = business.categories
+        }
+    }
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+
+}
--- Yelp/Yelp/View Controllers/BusinessesViewController.swift
+++ Yelp/Yelp/View Controllers/BusinessesViewController.swift
@@ -12,19 +12,40 @@ class BusinessesViewController: UIViewController {
 
     var businesses: [Business]!
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var noResultLabel: UILabel!
+    
+    var searchBar = UISearchBar()
+    var activityIndicatorView = UIActivityIndicatorView()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        searchBar.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 120
+        noResultLabel.isHidden = true
+        
+        
+        
+        searchBar.sizeToFit()
+//        navigationItem.titleView = searchBar
+        navigationController?.navigationBar.addSubview(searchBar)
 
-        Business.search(with: "Thai") { (businesses: [Business]?, error: Error?) in
+        Business.search(with: "") { (businesses: [Business]?, error: Error?) in
             if let businesses = businesses {
                 self.businesses = businesses
 
                 for business in businesses {
-                    print(business.name!)
-                    print(business.address!)
+                    self.businesses.append(business)
+                    self.tableView.reloadData()
                 }
             }
         }
+        
 
         // Example of Yelp search with more search options specified
         /*
@@ -40,5 +61,70 @@ class BusinessesViewController: UIViewController {
         }
         */
     }
+    
+    func onSearch() {
+        Business.search(with: searchBar.text!) { (result, error) in
+            if (result != nil) {
+                if (result!.count) > 0 {
+                    self.noResultLabel.isHidden = true
+                    self.businesses = result!
+                    self.tableView.isHidden = false
+                    self.tableView.reloadData()
+                } else {
+                    self.noResultLabel.isHidden = false
+                    self.tableView.isHidden = true
+                }
+            }else {
+                self.noResultLabel.isHidden = false
+                self.tableView.isHidden = true
+            }
+            
+        }
+    }
+    
 
+}
+
+
+
+
+
+
+// MARK: - Table View
+extension BusinessesViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return businesses?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "businessCell") as! BusinessCell
+        
+        cell.business = businesses[indexPath.row]
+        cell.restaurantNameLabel.text = String(indexPath.row + 1) + ". " + businesses[indexPath.row].name!
+        return cell
+    }
+}
+
+
+// MARK: - Search Bar
+extension BusinessesViewController: UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = true
+
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        onSearch()
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        businesses.removeAll()
+        onSearch()
+        searchBar.resignFirstResponder()
+    }
+    
 }
